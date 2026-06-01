cd477ec779f5355fccb16ee8a46b1876cd397542
lakshchhabra <lakshchhabra@localhost>
2026-06-01 14:08:30 -0400

    Initial Commit

---
+++ src/main/java/com/mycompany/projectlaksh_june1/ProjectLaksh_June1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectlaksh_june1;
+
+/**
+ *
+ * @author Laksh Chhabra, 2026
+ */
+public class ProjectLaksh_June1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
